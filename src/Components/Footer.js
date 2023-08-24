6b5418ca4b078d8138cec9a50f09e9bd9e3bf82e
MalcevStepan <stiepan-maltsiev@mail.ru>
2023-08-24 13:02:19 +0300

    fix links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import {Col, Row} from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import { useMediaQuery } from 'react-responsive';
 
 function Footer() {
@@ -8,33 +8,33 @@ function Footer() {
 
     const mobile = useMediaQuery({ query: '(max-width: 767px)' });
 
-    useEffect( () => {
+    useEffect(() => {
         const today = new Date();
         setYear(today.getFullYear());
     }, [])
 
-    return(
+    return (
 
-        <div style={{ backgroundColor: '#484545', paddingTop: '3rem', paddingBottom: '3rem'}} >
-            <Row style={{width: '100%'}} >
+        <div style={{ backgroundColor: '#484545', paddingTop: '3rem', paddingBottom: '3rem' }} >
+            <Row style={{ width: '100%' }} >
                 <Col sm={6} md={7} lg={8} >
-                 <div className='footer_div' style={{marginLeft: '7%'}} >
-                   © 1921—{year} Учреждение образования «Белорусский государственный медицинский университет».
-                 </div>
+                    <div className='footer_div' style={{ marginLeft: '7%' }} >
+                        © 1921—{year} Учреждение образования «Белорусский государственный медицинский университет».
+                    </div>
                 </Col>
                 <Col sm={6} md={5} lg={4} >
-                  <div style={
-                      mobile
-                      ? {marginLeft: '7%', marginTop: '1rem'}
-                      : {marginTop: '0px'}
-                  }>
-                      <a className='footer_link' rel="noreferrer" target='_blank' href="https://www.bsmu.by/page/18/1481/">Контактная информация</a>
-                     <div className='footer_div' >220083, г. Минск, пр. Дзержинского, 83</div>
-                     <div className='footer_div' >Тел: +375 17 252-12-01. Факс: +375 17 348-12-02</div>
-                   </div>
+                    <div style={
+                        mobile
+                            ? { marginLeft: '7%', marginTop: '1rem' }
+                            : { marginTop: '0px' }
+                    }>
+                        <a className='footer_link' rel="noreferrer" target='_blank' href="https://www.bsmu.by/kontakty/">Контактная информация</a>
+                        <div className='footer_div' >220083, г. Минск, пр. Дзержинского, 83</div>
+                        <div className='footer_div' >Тел: +375 17 252-12-01. Факс: +375 17 348-12-02</div>
+                    </div>
                 </Col>
             </Row>
-            
+
         </div>
     );
 }
